68e881e3105e238bc0b6897b29dae65c87872c2b
Pablo Nuñez <fixwah@gmail.com>
2017-07-04 16:10:07 -0300

    * Update Ruby * Update NodeJS and NPM * Fix permission issues in NPM global installs * Update android SDK filtered to V26 * Fix issues in PATH * Increased the VM memory * Enabled SSH forward (to use ssh keys) * Provision inline to copy the local git-configurations and SSH keys

--- Vagrantfile
+++ Vagrantfile
@@ -8,11 +8,16 @@ Vagrant.configure("2") do |config|
   config.vm.provision :shell, path: "bootstrap.sh"
   config.vm.network "forwarded_port", guest: 8100, host: 8100
   config.vm.network "forwarded_port", guest: 35729, host: 35729
+  config.ssh.forward_agent = true
+
+  config.vm.provision :shell, :inline => "echo -e '#{File.read("#{Dir.home}/.gitconfig")}' > '/home/vagrant/.gitconfig'"
+  config.vm.provision :shell, :inline => "echo -e '#{File.read("#{Dir.home}/.ssh/id_rsa")}' > '/home/vagrant/.ssh/id_rsa'"
 
   config.vm.synced_folder "ionic-projects/", "/ionic-projects", create: true
     config.vm.provider "virtualbox" do |vb|
       vb.name = "IonicVagrant"
       vb.gui = false
+      vb.memory = "3584"
       vb.customize ["modifyvm", :id, "--usb", "on"]
       #vb.customize ["usbfilter", "add", "0", "--target", :id, "1197123b", "--vendorid", "0x04e8"]
       vb.customize ["usbfilter", "add", "0", "--target", :id, "--name", "android", "--vendorid", "0x18d1"]
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 
 dpkg --configure -a
 dpkg --add-architecture i386
+apt-add-repository ppa:brightbox/ruby-ng -y
 
 apt-get update
 apt-get upgrade
@@ -21,15 +22,21 @@ apt-get install -y lib32stdc++6
 apt-get install -y libc6:i386
 apt-get install -y libstdc++6:i386
 apt-get install -y zlib1g:i386
+apt-get remove ruby
+apt-get remove nodejs
+apt-get remove ^node-*
+apt-get remove nodejs-*
+apt-get install ruby2.3 ruby2.3-dev
+apt-get autoremove
 apt-get autoclean
+gem update
 
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-npm install -g npm
-gem install sass
-sudo npm install --unsafe-perm -g node-gyp
-sudo npm install --unsafe-perm -g node-sass
+curl https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | sh
+mkdir /home/vagrant/npm-global
+npm config set prefix /home/vagrant/npm-global --global
 
 wget http://dl.google.com/android/android-sdk_r24.2-linux.tgz
 tar -xvf android-sdk_r24.2-linux.tgz
@@ -37,18 +44,27 @@ sudo chown -R vagrant android-sdk-linux/
 
 expect -c '
 set timeout -1   ;
-spawn /home/vagrant/android-sdk-linux/tools/android update sdk -u --all
+spawn /home/vagrant/android-sdk-linux/tools/android update sdk -u --all --filter platform-tool,android-26,build-tools-26
 expect {
     "Do you accept the license" { exp_send "y\r" ; exp_continue }
     eof
 }
 '
 
+
+
 echo "ANDROID_HOME=~/android-sdk-linux" >> /home/vagrant/.bashrc
-echo "export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64/" >> /home/vagrant/.zshrc
-echo "PATH=${PATH}:$HOME/android-sdk-linux/platform-tools/:$HOME/android-sdk-linux/tools:$HOME/android-sdk-linux/build-tools/23.0.1/" >> /home/vagrant/.zshrc
+echo "export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64/" >> /home/vagrant/.profile
+echo "PATH=${PATH}:$HOME/android-sdk-linux/platform-tools/:$HOME/android-sdk-linux/tools:$HOME/android-sdk-linux/build-tools/23.0.1/:/home/vagrant/npm-global/bin" >> /home/vagrant/.profile
 
-source /home/vagrant/.zshrc
 
+source /home/vagrant/.profile
+
+npm install npm@latest -g
+npm install -g uuid
+npm install -g node-gyp
+npm install -g node-sass
 npm install -g cordova
 npm install -g ionic
+chown -R vagrant /home/vagrant/npm-global
+chmod -R 777 /home/vagrant/npm-global
